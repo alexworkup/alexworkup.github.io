66db297e54daedca4c51b4801b5096948b15fbd7
alexworkup <arisalexfront@gmail.com>
2022-06-14 15:45:42 +0500

    Add main slider

---
+++ komilfo/js/script.js
@@ -0,0 +1,12 @@
+
+$( document ).ready(function() {
+
+    const mainSlider = new Swiper('.main-slider__wrapper.swiper-wrapper', {
+        loop: true,
+
+        pagination: {
+            el: '.main-slider .swiper-pagination',
+        },
+
+    });
+});
