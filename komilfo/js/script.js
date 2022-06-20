7c03d180768f9f08139dd40a37b53b6b52d1651e
alexworkup <arisalexfront@gmail.com>
2022-06-20 13:39:43 +0500

    Add block map

--- komilfo/js/script.js
+++ komilfo/js/script.js
@@ -1,6 +1,9 @@
 
 $( document ).ready(function() {
 
+
+    // Слайдеры
+
     const mainSlider = new Swiper('.main-slider.swiper', {
         loop: true,
         effect: "fade",
@@ -25,4 +28,37 @@ $( document ).ready(function() {
             prevEl: '.our-clients .slider-arrow--prev',
         },
     });
+
+    const cards = new Swiper('.card-slider .swiper', {
+        slidesPerView: 4,
+
+        navigation: {
+            nextEl: '.card-slider .slider-arrow--next',
+            prevEl: '.card-slider .slider-arrow--prev',
+        },
+    });
+
+    // Слайдеры
+
+
+    // Яндекс карта
+    ymaps.ready(function () {
+        var map = new ymaps.Map("map", {
+            center: [55.648484, 37.501950],
+            zoom: 15
+        });
+
+        if (map) {
+
+            map.geoObjects
+                .add(new ymaps.Placemark([55.648484, 37.501950], {
+                    balloonContent: 'Комильфо сервис<br> <a href="tel:+74957237444">8 (495) 723-74-44</a>'
+                }, {
+                    preset: 'islands#icon',
+                    iconColor: '#0095b6'
+                }))
+
+        }
+    });
+
 });
