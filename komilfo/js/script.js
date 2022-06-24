b5eafd0b518cbe920c5359d5ce56f8f0e18bf29f
alexworkup <arisalexfront@gmail.com>
2022-06-24 14:15:18 +0500

    Modified card

--- komilfo/js/script.js
+++ komilfo/js/script.js
@@ -70,4 +70,98 @@ $( document ).ready(function() {
         }
     });
 
+
+    // Стилизация select
+
+
+    if($('.prop-select').length) {
+
+        $('.prop-select').each(function() {
+            const _this = $(this),
+                selectOption = _this.find('option'),
+                selectOptionLength = selectOption.length,
+                selectedOption = selectOption.filter(':selected'),
+                duration = 250; // длительность анимации
+
+            _this.hide();
+            _this.wrap('<div class="prop-select"></div>');
+            $('<div>', {
+                class: 'new-select',
+                text: _this.children('option:disabled').text()
+            }).insertAfter(_this);
+
+            const selectHead = _this.next('.new-select');
+
+            selectHead.text( selectedOption[0].innerHTML );
+
+            // console.log(selectedOption[0].innerHTML);
+
+            $('<div>', {
+                class: 'new-select__list'
+            }).insertAfter(selectHead);
+
+            const selectList = selectHead.next('.new-select__list');
+            for (let i = 1; i < selectOptionLength; i++) {
+                $('<div>', {
+                    class: 'new-select__item',
+                    html: $('<span>', {
+                        text: selectOption.eq(i).text()
+                    })
+                })
+                    .attr('data-value', selectOption.eq(i).val())
+                    .appendTo(selectList);
+            }
+
+            const selectItem = selectList.find('.new-select__item');
+            selectList.slideUp(0);
+
+            selectHead.on('click', function() {
+                if ( !$(this).hasClass('on') ) {
+                    $(this).addClass('on');
+                    selectList.slideDown(duration);
+
+                    selectItem.on('click', function() {
+                        let chooseItem = $(this).data('value');
+
+                        $('select').val(chooseItem).attr('selected', 'selected');
+                        selectHead.text( $(this).find('span').text() );
+
+                        selectList.slideUp(duration);
+                        selectHead.removeClass('on');
+                    });
+
+                } else {
+                    $(this).removeClass('on');
+                    selectList.slideUp(duration);
+                }
+            });
+
+            $(document).click(function (e) {
+
+                if ( !$(selectHead).hasClass('on') ) {
+                    $(selectHead).addClass('on');
+                    selectList.slideDown(duration);
+
+                    selectItem.on('click', function() {
+                        let chooseItem = $(this).data('value');
+
+                        $('select').val(chooseItem).attr('selected', 'selected');
+                        selectHead.text( $(this).find('span').text() );
+
+                        selectList.slideUp(duration);
+                        selectHead.removeClass('on');
+                    });
+
+                } else {
+                    $(selectHead).removeClass('on');
+                    selectList.slideUp(duration);
+                }
+
+            });
+
+        });
+
+    }
+
+
 });
