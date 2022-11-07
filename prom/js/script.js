4e241c1978ca8d7a8bc334f7b6c6c08b97bba53b
alexworkup <arisalexfront@gmail.com>
2022-11-07 22:31:44 +0500

    Add product list

--- prom/js/script.js
+++ prom/js/script.js
@@ -1,3 +1,23 @@
-window.addEventListener('scroll', function() {
-    console.log('2324');
+var isMobile = false;
+$(document).ready(function () {
+
+    if ($(window).width() <= 768) {
+        isMobile = true;
+    }
+
+    var header = $('.header');
+
+    if(header.offset().top >= 100 && !isMobile) {
+        header.addClass('header--color');
+    } else {
+        header.removeClass('header--color');
+    }
+
+    $(window).scroll(function () {
+        if($(window).scrollTop() >= 100 && !isMobile) {
+            header.addClass('header--color');
+        } else {
+            header.removeClass('header--color');
+        }
+    });
 });
