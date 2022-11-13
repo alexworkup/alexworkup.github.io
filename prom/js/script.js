4afe0c59381e381de1dee7dddb3477456c5ac5be
alexworkup <arisalexfront@gmail.com>
2022-11-13 19:33:26 +0500

    Add few pages

--- prom/js/script.js
+++ prom/js/script.js
@@ -143,4 +143,27 @@ $(document).ready(function () {
             easing: "swing"
         });
     });
+
+    /* YAMAP */
+    var map_container = document.getElementById('map');
+    var options_map = {
+        once: true,
+        passive: true,
+        capture: true
+    };
+    map_container.addEventListener('click', start_lazy_map, options_map);
+    map_container.addEventListener('mouseover', start_lazy_map, options_map);
+    map_container.addEventListener('touchstart', start_lazy_map, options_map);
+    map_container.addEventListener('touchmove', start_lazy_map, options_map);
+
+    var map_loaded = false;
+    function start_lazy_map() {
+        if (!map_loaded) {
+            var map_block = document.getElementById('ymap_lazy');
+            map_loaded = true;
+            map_block.setAttribute('src', map_block.getAttribute('data-src'));
+            map_block.removeAttribute('data_src');
+        }
+    }
+    /* YAMAP */
 });
