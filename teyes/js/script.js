6d70f87b71b3ba9978dd308ae71d521b01bc05ad
alexworkup <arisalexfront@gmail.com>
2024-01-31 15:42:13 +0500

    add teyes

--- teyes/js/script.js
+++ teyes/js/script.js
@@ -6,12 +6,50 @@ $(function() {
         isMobile = true;
     }
 
-    const elementToggle = document.querySelectorAll('.js-toggle, .js-toggle-box, .js-toggle-close');
+    const buttonsToggle = document.querySelectorAll('.js-toggle-btn, .js-toggle-close');
+    const boxToggle = document.querySelector('.js-toggle-box');
+    const body = document.querySelector('body');
 
-    elementToggle.forEach((item) => {
-        console.log(item);
-        item.addEventListener('click', ()=> {
-            this.classList.toggle('active');
+    if(boxToggle) {
+
+        buttonsToggle.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                boxToggle.classList.toggle('active');
+
+                if(isMobile) {
+                    body.classList.toggle('open-burger');
+                }
+
+                e.preventDefault();
+                buttonsToggle.forEach((el) => {
+                    el.classList.toggle('active');
+                });
+            });
         })
-    });
+    }
+
+    const bigGallery = document.querySelector('.js-big-gallery');
+    const smallGallery = document.querySelector('.js-small-gallery');
+
+    let swiperBigGallery, swiperSmallGallery;
+
+    if(smallGallery) {
+        swiperSmallGallery = new Swiper(smallGallery, {
+            spaceBetween: 5,
+            slidesPerView: 5,
+            watchSlidesProgress: true,
+        });
+    }
+
+    if(bigGallery) {
+        swiperBigGallery = new Swiper(bigGallery, {
+            spaceBetween: 10,
+            slidesPerView: 1,
+            effect: 'fade',
+            thumbs: {
+                swiper: swiperSmallGallery,
+            },
+        });
+    }
+
 });
