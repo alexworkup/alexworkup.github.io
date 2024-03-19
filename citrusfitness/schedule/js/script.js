1d0211f7247b53d0654b64b8374f34ca6acd7992
alexworkup <arisalexfront@gmail.com>
2024-03-19 15:50:24 +0500

    add shedule

--- citrusfitness/schedule/js/script.js
+++ citrusfitness/schedule/js/script.js
@@ -26,18 +26,102 @@ window.addEventListener('DOMContentLoaded', ()=> {
 
         weekday.forEach((itemDay)=> {
             itemDay.addEventListener('click', () => {
-                itemDay.classList.toggle('active');
+                itemDay.classList.toggle('current');
                 const dataColumn = itemDay.getAttribute('data-column');
                 const columnItems = document.querySelectorAll('[data-column="'+dataColumn+'"]:not(.f-date)');
 
                 if(columnItems.length > 0) {
 
                     columnItems.forEach((item)=> {
-                        item.classList.toggle('active');
+                        item.classList.toggle('current');
                     });
                 }
             });
         });
     }
 
+    const scheduleHead = document.querySelector('.schedule-head__slider');
+    const scheduleBody = document.querySelector('.schedule-body__wrap');
+
+    let scheduleBodySlider;
+
+    if(scheduleHead) {
+        const scheduleHeadSlider = new Swiper(scheduleHead, {
+            slidesPerView: 7,
+            navigation: {
+                nextEl: '.f-arrow_type_right',
+                prevEl: '.f-arrow_type_left',
+            },
+            on: {
+                slideChange: function() {
+                    if(scheduleBodySlider) {
+                        scheduleBodySlider.slideTo(this.activeIndex);
+                    }
+                }
+            }
+        });
+    }
+
+    if(scheduleBody) {
+        scheduleBodySlider = new Swiper(scheduleBody, {
+            slidesPerView: 7,
+            allowTouchMove: false,
+            on: {
+                slideChange: function() {
+                    if(scheduleHeadSlider) {
+                        scheduleHeadSlider.slideTo(this.activeIndex);
+                    }
+                }
+            }
+        });
+    }
+
+});
+
+window.addEventListener('load', ()=> {
+
+    function debounce(func, wait, immediate) {
+        var timeout;
+        return function() {
+            var context = this, args = arguments;
+            var later = function() {
+                timeout = null;
+                if (!immediate) func.apply(context, args);
+            };
+            var callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if (callNow) func.apply(context, args);
+        };
+    }
+
+    function adjustHeights() {
+
+        const timeGroups = {};
+        document.querySelectorAll('.schedule-body__time').forEach(element => {
+            const time = element.dataset.time;
+            if (!timeGroups[time]) {
+                timeGroups[time] = [];
+            }
+            timeGroups[time].push(element);
+        });
+
+        Object.keys(timeGroups).forEach(time => {
+            let maxHeight = 0;
+            timeGroups[time].forEach(element => {
+                element.style.height = '';
+                if (element.offsetHeight > maxHeight) {
+                    maxHeight = element.offsetHeight;
+                }
+            });
+            timeGroups[time].forEach(element => {
+                element.style.height = `${maxHeight}px`;
+            });
+            document.querySelectorAll(`.f-time[data-time="${time}"]`).forEach(fTimeElement => {
+                fTimeElement.style.height = `${maxHeight}px`;
+            });
+        });
+    }
+    adjustHeights();
+    window.addEventListener('resize', debounce(adjustHeights, 150));
 });
