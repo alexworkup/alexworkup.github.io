98a6b4e8c78efe058ce574ba5994428014375cfa
alexworkup <arisalexfront@gmail.com>
2024-03-21 15:53:41 +0500

    add shedule

--- citrusfitness/schedule/js/script.js
+++ citrusfitness/schedule/js/script.js
@@ -1,7 +1,14 @@
+let isMobile = false;
+
 window.addEventListener('DOMContentLoaded', ()=> {
+
+    if (window.innerWidth <= 767) {
+        isMobile = true;
+    }
+
     const sortInner = document.querySelectorAll('.sort-popup__inner');
     const sortItems = document.querySelectorAll('.sort-item');
-    const weekday = document.querySelectorAll('.f-date');
+    const weekday = document.querySelectorAll('.schedule-head .f-col');
 
     if(sortInner.length > 0) {
 
@@ -22,60 +29,132 @@ window.addEventListener('DOMContentLoaded', ()=> {
         });
     }
 
-    if(weekday.length > 0) {
-
-        weekday.forEach((itemDay)=> {
+    if (weekday.length > 0) {
+        weekday.forEach((itemDay) => {
             itemDay.addEventListener('click', () => {
-                itemDay.classList.toggle('current');
-                const dataColumn = itemDay.getAttribute('data-column');
-                const columnItems = document.querySelectorAll('[data-column="'+dataColumn+'"]:not(.f-date)');
 
-                if(columnItems.length > 0) {
+                weekday.forEach((item) => {
+                    item.classList.remove('current');
+                });
 
-                    columnItems.forEach((item)=> {
-                        item.classList.toggle('current');
+                itemDay.classList.add('current');
+
+                const dataColumn = itemDay.getAttribute('data-match-index');
+                const columnItems = document.querySelectorAll('[data-match-index="' + dataColumn + '"]:not(.f-col-date)');
+
+                if (columnItems.length > 0) {
+                    columnItems.forEach((item) => {
+                        document.querySelectorAll('[data-match-index]:not(.f-col-date)').forEach((colItem) => {
+                            colItem.classList.remove('current');
+                        });
+                        item.classList.add('current');
                     });
                 }
             });
         });
     }
 
+
     const scheduleHead = document.querySelector('.schedule-head__slider');
     const scheduleBody = document.querySelector('.schedule-body__wrap');
 
     let scheduleBodySlider;
+    let scheduleHeadSlider;
 
     if(scheduleHead) {
-        const scheduleHeadSlider = new Swiper(scheduleHead, {
+
+        scheduleHeadSlider = new Swiper(scheduleHead, {
             slidesPerView: 7,
+            breakpoints: {
+                768: {
+                    slidesPerView: 3,
+                },
+                980: {
+                    slidesPerView: 5,
+                },
+                1460: {
+                    slidesPerView: 7,
+                }
+            },
             navigation: {
                 nextEl: '.f-arrow_type_right',
                 prevEl: '.f-arrow_type_left',
             },
             on: {
                 slideChange: function() {
-                    if(scheduleBodySlider) {
+                    if(scheduleBodySlider && !isMobile) {
                         scheduleBodySlider.slideTo(this.activeIndex);
                     }
-                }
+                },
+
+                click: function () {
+                  if(isMobile) {
+                      let slideIndex = this.clickedSlide.getAttribute('data-match-index');
+                      scheduleBodySlider.slideTo(slideIndex - 1);
+                  }
+                },
             }
         });
     }
 
     if(scheduleBody) {
         scheduleBodySlider = new Swiper(scheduleBody, {
-            slidesPerView: 7,
-            allowTouchMove: false,
+            slidesPerView: 1,
+            allowTouchMove: true,
+            breakpoints: {
+                768: {
+                    slidesPerView: 3,
+                    allowTouchMove: false,
+                },
+                980: {
+                    slidesPerView: 5,
+                    allowTouchMove: false,
+                },
+                1460: {
+                    slidesPerView: 7,
+                    allowTouchMove: false,
+                }
+            },
             on: {
                 slideChange: function() {
-                    if(scheduleHeadSlider) {
+                    if(scheduleHeadSlider && isMobile) {
                         scheduleHeadSlider.slideTo(this.activeIndex);
+                        let slideIndex = this.activeIndex + 1;
+                        weekday.forEach((item) => {
+                            item.classList.remove('current');
+                        });
+                        document.querySelector('.f-col-date[data-match-index="' + slideIndex + '"]').classList.add('current');
                     }
                 }
             }
         });
     }
 
+    if(isMobile) {
+
+        const monthsMap = {
+            'января': '01', 'февраля': '02', 'марта': '03', 'апреля': '04', 'мая': '05', 'июня': '06',
+            'июля': '07', 'августа': '08', 'сентября': '09', 'октября': '10', 'ноября': '11', 'декабря': '12'
+        };
+
+        const daysMap = {
+            'понедельник': 'пн', 'вторник': 'вт', 'среда': 'ср', 'четверг': 'чт',
+            'пятница': 'пт', 'суббота': 'сб', 'воскресенье': 'вс'
+        };
+
+        const dateElements = document.querySelectorAll('.f-col-date');
+
+        dateElements.forEach(element => {
+            const dayPart = element.querySelector('.f-date i').textContent;
+            const datePart = element.querySelector('.f-date span').textContent.trim().split(' ');
+
+            const dayShort = daysMap[dayPart.toLowerCase()];
+            const monthShort = monthsMap[datePart[1].toLowerCase()];
+            const dateShort = `${datePart[0]}.${monthShort}`;
+
+            element.querySelector('.f-date').innerHTML = `${dayShort}<br><span>${dateShort}</span>`;
+        });
+    }
 });
 
 window.addEventListener('load', ()=> {
@@ -122,6 +201,9 @@ window.addEventListener('load', ()=> {
             });
         });
     }
-    adjustHeights();
-    window.addEventListener('resize', debounce(adjustHeights, 150));
+
+    if(!isMobile) {
+        adjustHeights();
+        window.addEventListener('resize', debounce(adjustHeights, 150));
+    }
 });
