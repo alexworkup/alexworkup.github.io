15a55013327d96d0363eb5068f4925b3b9a0cc85
alexworkup <arisalexfront@gmail.com>
2024-04-02 15:41:27 +0500

    add shedule

---
+++ citrus/js/script.js
@@ -0,0 +1,20 @@
+window.addEventListener('DOMContentLoaded', ()=> {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const screens = document.querySelectorAll('.screen');
+
+    if(screens.length > 1) {
+
+        gsap.utils.toArray(".screen").forEach((screen, i) => {
+            ScrollTrigger.create({
+                trigger: screen,
+                start: "top top",
+                pin: true,
+                pinSpacing: false,
+                scrub: 1,
+            });
+        });
+    }
+
+});
