8e4e07b4fb509db0dc644b8f094f633e2f5cec3b
alexworkup <arisalexfront@gmail.com>
2024-04-14 22:46:34 +0500

    edit teyes

--- citrus/js/script.js
+++ citrus/js/script.js
@@ -1,5 +1,60 @@
 window.addEventListener('DOMContentLoaded', ()=> {
 
+    const popupContent = document.querySelector('#popupContent');
+    const itemVacancy = document.querySelectorAll('#listVacancies .team-section__item');
+    const closeBtnFancy = document.querySelectorAll('.js-close-fancy');
+    const burgerBtn = document.querySelectorAll('.burger');
+    const popupDropdown = document.querySelector('#popupDropdown');
+
+    if(burgerBtn.length > 0) {
+
+        burgerBtn.forEach(item => {
+
+            item.addEventListener('click', (e) => {
+
+                Fancybox.show(
+                    [
+                        {
+                            src: popupDropdown,
+                            type: "inline"
+                        }
+                    ],
+                    {
+                        mainClass: 'open-popup-content'
+                    });
+            });
+        });
+
+    }
+
+    if(closeBtnFancy.length > 0) {
+        closeBtnFancy.forEach(item => {
+            item.addEventListener('click', (e) => {
+                Fancybox.close();
+            });
+        });
+    }
+
+    if(itemVacancy.length > 0) {
+        itemVacancy.forEach(item => {
+            item.addEventListener('click', (e) => {
+                e.preventDefault();
+
+                Fancybox.show(
+                    [
+                        {
+                            src: popupContent,
+                            type: "inline"
+                        }
+                    ],
+                    {
+                        mainClass: 'open-popup-content'
+                    });
+            });
+
+        });
+    }
+
     gsap.registerPlugin(ScrollTrigger);
 
     const screens = document.querySelectorAll('.screen');
